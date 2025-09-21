96dd93f8f3f51be9a878b1b9deb058b8f337dbc2
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2025-09-21 23:02:47 +0300

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using System;
+
+namespace BOSpecialFools
+{
+    [BepInPlugin(MOD_GUID, MOD_NAME, MOD_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string MOD_GUID = "SpecialAPI.BOSpecialFools";
+        public const string MOD_NAME = "SpecialAPI's Fools";
+        public const string MOD_VERSION = "0.0.0";
+
+        public void Awake()
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# BOSpecialFools
+A mod for Brutal Orchestra that adds new characters to the game.
