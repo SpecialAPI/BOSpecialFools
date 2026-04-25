bf0f0531a4137bda8cfa31f29bf20c10490473df
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-04-25 13:36:43 +0300

    Add CheckTargetsHealthCompareToEntryEffect

---
+++ Effect/CheckTargetsHealthCompareToEntryEffect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOSpecialFools.Effect
+{
+    public class CheckTargetsHealthCompareToEntryEffect : CheckTargetsEffectBase
+    {
+        public IntComparison comparison = IntComparison.LessThanOrEqual;
+
+        public override bool CheckUnit(IUnit target, CombatStats stats, IUnit caster, bool areTagetSlots, int entryVariable, ref int exitAmount)
+        {
+            if (!CompareInts(target.CurrentHealth, entryVariable, comparison))
+                return false;
+
+            exitAmount++;
+            return true;
+        }
+    }
+}
