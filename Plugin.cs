79301bd49ea61967f1b41a77db0bacb9233c815f
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-05-02 16:31:07 +0300

    Bump plugin version for the playtest build

--- Plugin.cs
+++ Plugin.cs
@@ -9,7 +9,7 @@ namespace BOSpecialFools
     {
         public const string MOD_GUID = "157.Fools";
         public const string MOD_NAME = "157 FOOLS";
-        public const string MOD_VERSION = "0.0.0";
+        public const string MOD_VERSION = "0.0.1";
         public const string MOD_PREFIX = "157Fools";
 
         public static Harmony HarmonyInstance;
