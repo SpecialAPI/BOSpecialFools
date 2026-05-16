4cb293d79db1a795381246eefa480d24ce131505
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-05-16 23:25:33 +0300

    UnitTypes class

---
+++ Tools/UnitTypes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOSpecialFools.Tools
+{
+    public static class UnitTypes
+    {
+        public static readonly string Fish              = UnitType_GameIDs.Fish.ToString();
+        public static readonly string FemaleLooking     = UnitType_GameIDs.FemaleLooking.ToString();
+
+        public static readonly string FemaleID  = "FemaleID";
+        public static readonly string MaleID    = "MaleID";
+
+        public static readonly string Bird          = "Bird";
+        public static readonly string Robot         = "Robot";
+        public static readonly string Alien         = "Alien";
+        public static readonly string HellishID     = "HellishID";
+        public static readonly string Angel         = "Angel";
+
+        public static readonly string Sandwich_War              = "Sandwich_War";
+        public static readonly string Sandwich_Fire             = "Sandwich_Fire";
+        public static readonly string Sandwich_Spirit           = "Sandwich_Spirit";
+        public static readonly string Sandwich_Gore             = "Sandwich_Gore";
+        public static readonly string Sandwich_Silly            = "Sandwich_Silly";
+        public static readonly string Sandwich_Pigment          = "Sandwich_Pigment";
+        public static readonly string Sandwich_Fish             = "Sandwich_Fish";
+        public static readonly string Sandwich_SuperOrganism    = "Sandwich_SuperOrganism";
+        public static readonly string Sandwich_Robot            = "Sandwich_Robot";
+        public static readonly string Sandwich_BDSM             = "Sandwich_BDSM";
+        public static readonly string Sandwich_NULL             = "Sandwich_NULL";
+    }
+}
