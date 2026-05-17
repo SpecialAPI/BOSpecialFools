105a783583fe6a444c2e22263a8e2967fda7ea91
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-05-17 12:39:49 +0300

    NewRankedAbility

--- GlobalUsings/GlobalUsings_Local.cs
+++ GlobalUsings/GlobalUsings_Local.cs
@@ -6,4 +6,5 @@ global using BOSpecialFools.Tools;
 global using BOSpecialFools.TriggerEffects;
 
 global using static BOSpecialFools.Tools.LocalCombatTools;
+global using static BOSpecialFools.Tools.LocalAbilityTools;
 global using static BOSpecialFools.Plugin;
---
+++ Tools/LocalAbilityTools.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOSpecialFools.Tools
+{
+    public static class LocalAbilityTools
+    {
+        public static AdvancedAbilitySO NewRankedAbility(string[] ids) => NewAbility(RankedValue(ids), Profile);
+
+        public static T NewRankedAbility<T>(string[] ids) where T : AbilitySO => NewAbility<T>(RankedValue(ids), Profile);
+    }
+}
