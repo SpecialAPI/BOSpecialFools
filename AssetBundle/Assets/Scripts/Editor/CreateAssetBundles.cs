684e30bfda1c3ee2990714327abe4c7f997daba2
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-08-14 18:42:06 +0300

    Added asset bundle unity project

---
+++ AssetBundle/Assets/Scripts/Editor/CreateAssetBundles.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    private static void BuildAllAssetBundles()
+    {
+        var assetBundleDirectory = "AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+            Directory.CreateDirectory(assetBundleDirectory);
+
+        try
+        {
+            BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.StrictMode, BuildTarget.StandaloneWindows);
+
+            File.Delete($"{assetBundleDirectory}/AssetBundles");
+            var d = new DirectoryInfo(assetBundleDirectory);
+            foreach (var file in d.GetFiles("*.manifest"))
+                file.Delete();
+            foreach (var file in d.GetFiles("*.manifest.meta"))
+                file.Delete();
+
+            AssetDatabase.Refresh();
+            Debug.Log("<b>✔️ SUCCESSFULLY BUILDED ASSETBUNDLES ✔️</b>");
+        }
+        catch(Exception e)
+        {
+            Debug.LogError("AN ERROR OCCURED WHILE BUILDING THE ASSETBUNDLES!\n" + e.ToString());
+        }
+    }
+}
---
+++ AssetBundle/Assets/Scripts/ModdableData/ModdedRoomHandlerData/BaseRoomHandlerModData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseRoomHandlerModData : MonoBehaviour
+{
+    public abstract string RoomHandlerType { get; }
+
+}
+
+public enum RoomHandlerType_GameIDs
+{
+    NPC,
+    Fools,
+    Shop,
+    Enemy,
+    Boss,
+    MoneyChest,
+    Treasure,
+    Bar
+}
